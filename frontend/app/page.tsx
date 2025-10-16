3fa882cbbc67cf70c8e83811b1f41bc82fe7a19a
Daniel0455 <nielj455@gmail.com>
2025-10-16 15:06:01 +0300

    Mise à jour de contact

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home(){
                 <Projet/>
                 <Outil/>
                 <Langage/>
+                <Contact/>
                 <FuturisticBackground/>
             </div>
         </>
--- frontend/components/contact.tsx
+++ frontend/components/contact.tsx
@@ -1,32 +1,43 @@
 "use client"
-export default function Contact(){
+export default function Contact() {
     let contacts = [
         {
-            icon: "facebook",
-            coo: "Razafindranaivo Aimé Daniel Johnston"
+            icon: "fab fa-facebook",
+            couleur: "rgb(24, 119, 242)",
+            valeur: "Razafindranaivo Aimé Daniel Johnston"
         },
         {
-            icon: "WhatsApp",
-            coo: "+261342819583"
+            icon: "fab fa-whatsapp",
+            couleur: "rgb(37, 211, 102)",
+            valeur: "+261 34 28 195 83"
         },
         {
-            icon: "Email",
-            coo: "nielj455@gmail.com"
-        }
+            icon: "fas fa-envelope",
+            couleur: "rgb(227, 79, 38)",
+            valeur: "nielj455@gmail.com"
+        },
+        {
+            icon: "fab fa-linkedin-in",
+            couleur: "rgb(0, 119, 181)",
+            valeur: "Aimé Daniel Johnston RAZAFINDRANAIVO"
+        },
     ]
     return(
-        <>
-            <div className="containerContact">
-                <p className="titre">Contact</p>
-                <div className="containerContactMain">
-                    {contacts.map((contact) => (
-                        <div key={contact.icon}>
-                            <p>{contact.icon}</p>
-                            <p>{contact.coo}</p>
-                        </div>
-                    ))}
-                </div>
+        <div className="containercontact">
+            <p className="soustitre">Restons en <label className="daniel">Contact</label></p>
+            <p className="intropro">N'hésitez pas à me contacter pour discuter de vos projets ou simplement échanger.</p>
+            <div className="ligne"></div>
+            <div className="containerprojet">
+                {contacts.map((contact) =>(
+                    <div key={contact.icon} className="cardcontact">
+                        <i className={contact.icon} style={{color: contact.couleur, fontSize:'40px'}}></i>
+                        <p className='pgris'>{contact.valeur}</p>
+                    </div>
+                ))}
             </div>
-        </>
+            <div className="cdroit">
+                <p>2025 Daniel. Tous droits réservés. Créé avec ❤ et Next.js.</p>
+            </div>
+        </div>
     )
 }
